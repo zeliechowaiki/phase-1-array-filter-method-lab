113a52a67d8246d228f779a85439760a4045c497
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-02 11:24:15 -0500

    commit tree 9e07b444cb43f2c3358d4d2a400629bc1f8b61f7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (drivers, name) {
+    return drivers.filter(function(element) {
+        return element.toLowerCase() === name.toLowerCase();
+    })
+}
+function fuzzyMatch (drivers, name) {
+    return drivers.filter(function(element){
+        return (element.toLowerCase())[0] === (name.toLowerCase())[0];
+    })
+}
+function matchName (drivers, name) {
+    return drivers.filter(function(element){
+        return element.name === name;
+    })
+}
